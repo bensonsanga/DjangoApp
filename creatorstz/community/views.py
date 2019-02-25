37bae8e81f19b279cabe91da9f4f699a44139556
Benson Sanga <bensonsanga@hotmail.com>
2019-02-25 16:45:44 +0100

    Added regester form

--- creatorstz/community/views.py
+++ creatorstz/community/views.py
@@ -1,31 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Benson',
-        'title': 'Post 1',
-        'content': 'My first test post in django',
-        'date_posted': 'February 21 2019'
-    },
-    {
-        'author': 'Bosco',
-        'title': 'IDK',
-        'content': 'Hey there',
-        'date_posted': 'February 25 2019'
-    },
-    {
-        'author': 'Benjamin',
-        'title': 'ENGINES',
-        'content': 'I am an engineer',
-        'date_posted': 'February 11 2019'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'community/home.html', context)
 
--- creatorstz/creatorstz/settings.py
+++ creatorstz/creatorstz/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'community.apps.CommunityConfig',
     'business.apps.BusinessConfig',
     'front.apps.FrontConfig',
